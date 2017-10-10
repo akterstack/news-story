1f83316a624246ed6875fc08d83cee8cabf043eb
shg@dsi <akter.hossain@dsinnovators.com>
2017-10-11 00:50:34 +0600

    enabling global CORS

--- src/main/java/com/newsstories/Application.java
+++ src/main/java/com/newsstories/Application.java
@@ -2,12 +2,19 @@ package com.newsstories;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
 }
