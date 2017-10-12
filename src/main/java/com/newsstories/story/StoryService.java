b6c7918f4a69872fae4e54d0422af7111571b3bd
shg@dsi <akter.hossain@dsinnovators.com>
2017-10-13 02:23:47 +0600

    fixing timezone

--- src/main/java/com/newsstories/story/StoryService.java
+++ src/main/java/com/newsstories/story/StoryService.java
@@ -3,6 +3,7 @@ package com.newsstories.story;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.Instant;
 import java.util.Date;
 
 @Service
@@ -17,7 +18,7 @@ public class StoryService {
     @Transactional
     public Story save(Story story) {
         if (story.getPublishedDate() == null) {
-            story.setPublishedDate(new Date());
+            story.setPublishedDate(Date.from(Instant.now()));
         }
         return storyRepository.save(story);
     }
